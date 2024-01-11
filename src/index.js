2f3d8c096261e1fbe5c2d610e8ce7e4eba1e3eea
JonatanDrysen <jojodrysen@gmail.com>
2024-01-11 15:25:00 +0100

    add task 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const date = new Date()
 let userName = null
-let birthYear = null
+let userBirthYear = null
+let userAge = null
 
 function getUserName() {
     const promptString = String(window.prompt("Hello!, enter your name please.", "John Doe"))  
@@ -7,11 +9,19 @@ function getUserName() {
     userName ? console.log(`Hello, ${userName}!`) : userName = "Anonymous";
 }
 
-function getBirthYear() {
+function getUserBirthYear() {
     const promptString = Number(window.prompt("Enter your birth year please."))
-    birthYear = promptString
-    if(!birthYear) { birthYear = 2024 }
+    userBirthYear = promptString
+    if(!userBirthYear) { userBirthYear = date.getFullYear() }
 }
 
-getUserName()
-getBirthYear()
+function calculateAge() {
+    const currentYear = date.getFullYear()
+    const age = currentYear - userBirthYear
+    userAge = age
+    console.log(userAge)
+}
+
+getUserName
+getUserBirthYear
+calculateAge
